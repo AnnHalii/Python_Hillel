10e35619de083c6069492b02f0674bcfe0be2345
AnnHalii <halii.anna22@gmail.com>
2021-04-04 23:01:45 +0300

    upd 6 hw

--- lk_6_hw/5_task.py
+++ lk_6_hw/5_task.py
@@ -1,1 +1,13 @@
 import json
+def answer_questions(file_path):
+    with open(file_path) as file:
+        data = json.load(file)
+
+    for idx, q in enumerate(data.get('questions', [])):
+        data['questions'][idx]['answer'] = input(q['q'])
+
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+answer_questions('questions.json')
